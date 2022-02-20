938cbb9d6cd1b3b7f8234f486efa670591b14769
Catherine Tran <cathy_tran@CatherinesAir2.attlocal.net>
2022-02-20 00:59:00 -0800

    push project

--- MonthConverter_ArrayFun/src/main/java/ArrayFunReverseIt.java
+++ MonthConverter_ArrayFun/src/main/java/ArrayFunReverseIt.java
@@ -6,6 +6,7 @@ public class ArrayFunReverseIt {
         int[] originalArray = {1,2,3,4,5};
             System.out.println("This is the original order of the array.");
 
+        //declare reverse of originalArray
         int[] reverseReverse = new int[5];
 
         //use enhanced for loop to print originalArray
@@ -16,7 +17,7 @@ public class ArrayFunReverseIt {
         //display user input
         System.out.println("\n" + "This is the reverse order.");
 
-        //declare reverse of originalArray
+        //use for loop to print reverseReverse
         for (int i = 0; i < originalArray.length; i++) {
             reverseReverse[i] = originalArray[originalArray.length - 1 - i];
             System.out.println(reverseReverse[i]);
